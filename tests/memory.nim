c7434b396e4a1e336b835b8f95fd4c69cf3c1679
Erik O'Leary <erik.m.oleary@gmail.com>
2015-01-05 22:43:31 -0600

    Updated with working stackptr & 'new' taking objconstr. "StackPtr" and "stack" are temporary names

--- tests/memory.nim
+++ tests/memory.nim
@@ -2,44 +2,138 @@
 
 import macros
 
+# `New` macro
+proc makeRef(obj: PNimrodNode): PNimrodNode {.compiletime.} =
+    var typeName: string
+    var assignments = newSeq[PNimrodNode]()
+
+    for i in obj.children:
+        case i.kind
+        of nnkIdent:
+            typeName = $i
+        of nnkExprColonExpr:
+            var left, right: PNimrodNode
+            for value in i.children:
+                if left == nil: left  = value
+                else:           right = value
+            assignments.add newAssignment(
+                newDotExpr(ident"i", left),
+                right
+            )
+        else: discard
+
+    # Compose resulting AST
+    var resultExpr = newStmtList(
+        newNimNode(nnkVarSection).add(
+            newNimNode(nnkIdentDefs).add(
+                ident"i",
+                newNimNode(nnkRefTy).add(typeName.ident),
+                newEmptyNode()
+            )
+        ),
+        parseExpr("new(i)")
+    )
+
+    resultExpr.add(assignments)
+    resultExpr.add(ident"i")
+
+    return newStmtList(
+        newBlockStmt(newEmptyNode(), resultExpr))
+
+macro new*(obj: expr{nkObjConstr|nkCall}): expr =
+    makeRef(obj)
+
+# `Stack` macro
 type StackPtr*[T] = object
     get: ptr T
 
 method destroy*[T](obj: var StackPtr[T]) {.override.} =
     dealloc(obj.get)
-
     when isMainModule: echo "Destroyed"
 
 macro `.`*[T](left: StackPtr[T], right: expr): expr {.immediate.} =
     result = parseExpr($left & ".get." & right.strVal)
 
-template new*(obj: expr): expr =
-    # Allocate memory
-    var heapRef = cast[ptr type(obj)](alloc(sizeof(type(obj))))
+proc makePtr(obj: PNimrodNode): PNimrodNode {.compiletime.} =
+    var typeName: string
+    var assignments = newSeq[PNimrodNode]()
 
-    block:
-        # TODO: Replace this with more efficient mechanism; destruct
-        # the object construction into multiple assignments
+    for i in obj.children:
+        case i.kind
+        of nnkIdent:
+            typeName = $i
+        of nnkExprColonExpr:
+            var left, right: PNimrodNode
+            for value in i.children:
+                if left == nil: left  = value
+                else:           right = value
+            assignments.add newAssignment(
+                newDotExpr(ident"i", left),
+                right
+            )
+        else: discard
 
-        # Copy object onto heap
-        var stackRef = obj
-        copyMem(heapRef, addr stackRef, sizeof(stackRef))
+    # Compose resulting AST
+    var resultExpr = newStmtList(
+        newNimNode(nnkVarSection).add(
+            newNimNode(nnkIdentDefs).add(
+                ident"i",
+                newEmptyNode(),
+                parseExpr("cast[ptr " & typeName & "](alloc(sizeof(" & typeName & ")))"),
+            )
+        )
+    )
 
-    # Create container
-    StackPtr[type(obj)](get: heapRef)
+    resultExpr.add(assignments)
+    resultExpr.add parseExpr("StackPtr[" & typeName & "](get:i)")
 
+    return newStmtList(
+        newBlockStmt(newEmptyNode(), resultExpr))
+
+macro stack*(obj: expr{nkObjConstr|nkCall}): expr =
+    makePtr(obj)
+
+# Test `new`
 when isMainModule:
+    echo "Test `new`:"
 
     type MyType = object
-        id: int
         value: int
+        other: ref MyType
 
-    proc test2(obj: StackPtr[MyType]): string =
-        $obj.value
+    proc square(self: MyType|ref MyType): auto =
+        if self.other != nil:
+            self.other.value * self.other.value
+        else:
+            self.value * self.value
 
     proc test1 =
-        var item = new MyType(id: 5, value: 15)
-        echo item.id
-        echo item.test2
+        var item1 = MyType(value: 5)     # Creates: MyType
+        var item2 = new MyType(value: 5) # Creates: ref MyType
+        var item3 = new MyType(other: new MyType(value: 7))
+
+        assert(item2 != nil)
+        assert(item1.value == 5)
+        assert(item2.value == 5)
+
+        echo item1.square
+        echo item2.square
+        echo item3.square
+
+        assert(declared(i) == false, "`i` leaked to main scope")
 
     test1()
+
+# Test `stack`
+when true:
+    echo "Test `stack`:"
+
+    type MyObject = object
+        value: int
+
+    proc test2 =
+        var test1 = stack MyObject(value: 5)
+
+        echo test1.value
+
+    test2()
