47b6ed248dd1ee7e7d400dda3d44bfe52d1ef0e3
unknown <eoleary@EOLEARY-0112.corp.avtex.com>
2015-01-15 10:53:58 -0600

    Support ref or non-ref list

--- tests/list.nim
+++ tests/list.nim
@@ -12,22 +12,13 @@ type
         tail: ref Node[T]
         length: int
 
+    IList*[T] = List[T] | ref List[T]
+
 proc `$`*(node: ref Node): string =
     if node != nil: $(node.item)
     else: ""
 
-proc len*(list: List|ref List): int =
-    list.length
-
-proc `[]`*[T](list: ref List[T], index: int): ref Node[T] =
-    var i = 0
-    for node in list:
-        if i == index: return node
-        inc(i)
-
-    raise newException(IndexError, "Index out of range")
-
-proc `$`*(list: ref List): string =
+proc `$`*(list: IList): string =
     result = ""
     for i in list:
         result &= $i
@@ -35,13 +26,24 @@ proc `$`*(list: ref List): string =
         if i != list.tail:
             result &= ", "
 
-iterator items*[T](list: ref List[T]): ref Node[T] =
+proc len*(list: IList): int =
+    list.length
+
+proc `[]`*[T](list: IList[T], index: int): ref Node[T] =
+    var i = 0
+    for node in list:
+        if i == index: return node
+        inc(i)
+
+    raise newException(IndexError, "Index out of range")
+
+iterator items*[T](list: IList[T]): ref Node[T] =
     var node = list.head
     while node != nil:
         yield node
         node = node.next
 
-proc add*[T](list: ref List, value: T) =
+proc add*[T](list: var IList, value: T) =
     var node = new Node[T](item: value)
 
     if list.head == nil:
